f4f243370508a0c5bd13c9295eb011e7dcdc4882
kyosu-1 <sho013039@gmail.com>
2022-06-30 18:34:57 +0900

    impl main

--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module todoapp-in-go
 
 go 1.17
 
-require github.com/labstack/echo/v4 v4.7.2
+require (
+	github.com/jinzhu/gorm v1.9.16
+	github.com/labstack/echo v3.3.10+incompatible // indirect
+	github.com/labstack/echo/v4 v4.7.2
+)
--- main.go
+++ main.go
@@ -1,16 +1,20 @@
 package main
 
 import (
+	"todoapp-in-go/config"
+	"todoapp-in-go/repository/gorm"
+	"todoapp-in-go/usecase"
+	"todoapp-in-go/router"
+
 	"github.com/labstack/echo/v4"
 )
 
 func main() {
+	taskRepository := gorm.NewTaskRepository(config.NewDB())
+	taskUsecase := usecase.NewTaskUsecase(taskRepository)
+	taskHandler := router.NewTaskHandler(taskUsecase)
+
 	e := echo.New()
-	e.GET(
-		"/",
-		func(c echo.Context) error {
-			return c.String(200, "Hello, World!")
-		},
-	)
-	e.Logger.Fatal(e.Start(":8000"))
+	router.InitRouting(e, taskHandler)
+	e.Logger.Fatal(e.Start(":8080"))
 }
